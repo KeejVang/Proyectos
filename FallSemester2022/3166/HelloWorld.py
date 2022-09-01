2b9cd38addb7ebfdf45ab0fb8e0116aded8a393e
Eking <evang1@uncc.edu>
2022-09-01 15:37:19 -0400

    Added FallSemester2022 Files

---
+++ FallSemester2022/3166/HelloWorld.py
@@ -0,0 +1,11 @@
+# Use print function to print a String 
+print("This line will be printed.")
+
+#Indention
+x = 1
+if x == 1:
+    #4 Spaces needed for indention
+    print("x is 1.")
+
+# Exercise    
+print("Hello, World")
---
+++ FallSemester2022/3166/VariablesAndTypes.py
@@ -0,0 +1,51 @@
+#Variables and Types 
+
+# (Numbers)
+# Integers (whole numbers))
+myint = 7
+print(myint)
+
+# Floating point numbers (decimals)
+# Two types of notations
+
+# Notation 1
+myfloat = 7.1
+print(myfloat)
+# Notation 2
+myfloat = float(7)
+print(myfloat)
+
+# Strings
+# Can be defined with single or double quotes -> '' or ""
+mystring ='hello'
+print(mystring)
+mystring = "Hello"
+print(mystring)
+
+# Double Strings make it easy to include apostraphes
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+# Simple Operations can be executed on numbers and strings
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+hellowworld = hello + " " + world
+print(hellowworld)
+
+#Assignments can be done on more than one variable 
+# "simultaneously" on the same line:
+a, b = 3, 4
+print(a, b)
+
+#Mixing operators between numbers and strings are not supported
+# one = 1
+# two = 2
+# hello = "hello"
+# print(one + two + hello)
+
+#Exercise
